3a2a2228a76ce45e57e35c2fb9a2f1707fae4ab0
Lukasz Mielnikowski <whetold@hotmail.com>
2015-03-11 12:56:32 +0100

    init push

---
+++ lab02/NieudanyPsikusException.java
@@ -0,0 +1,12 @@
+package lab02;
+
+public class NieudanyPsikusException extends Exception  {
+
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NieudanyPsikusException() {}
+		
+}
---
+++ lab02/Psik.java
@@ -0,0 +1,20 @@
+package lab02;
+
+public class Psik implements Psikus {
+
+	public Integer cyfrokrad(Integer liczba) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer hultajchochla(Integer liczba) throws NieudanyPsikusException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer nieksztaltek(Integer liczba) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
