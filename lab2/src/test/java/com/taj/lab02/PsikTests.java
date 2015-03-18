fce92ef38e4c4b622bf9a657064ec993930370e7
Lukasz Mielnikowski <whetold@hotmail.com>
2015-03-18 16:34:22 +0100

    finish

--- lab2/src/test/java/com/taj/lab02/PsikTests.java
+++ lab2/src/test/java/com/taj/lab02/PsikTests.java
@@ -11,9 +11,8 @@ import org.junit.rules.ExpectedException;
 
 
 public class PsikTests {
-
-	 @Rule
-	 public ExpectedException thrown = ExpectedException.none();
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 	
 	public Psik psik;
 	
@@ -22,7 +21,44 @@ public class PsikTests {
        psik = new Psik();
    }
 	
+	@Test
+	public void CyfrokradTest1(){
+		Integer onenumber = psik.cyfrokrad(4);
+		assertNull("should return null",onenumber);
+	}
 	
-	
+	@Test
+	public void CyfrokradTest2(){
+		Integer onenumber = psik.cyfrokrad(9);
+		assertNull("should return null",onenumber);
+	}
 
+	 @Test
+	 public void CyfrokradTest3(){
+	 Integer result= psik.cyfrokrad(4413);
+	 assertEquals("Result should be one of following",result==441|| result==413 || result==443 );
+	 }
+	
+	
+	 @Test
+	 public void hultajchochlaTest() throws NieudanyPsikusException{
+	 Integer result = psik.hultajchochla(46);
+	 assertTrue(result.equals(64));
+	 }
+	 
+	 @Test
+	 public void hultajchochlaTest2() throws NieudanyPsikusException{
+	 thrown.expect(NieudanyPsikusException.class);
+	 psik.hultajchochla(1);
+	 }
+	 
+	 public void nieksztaltekTest() throws NieudanyPsikusException{
+		 Integer result=psik.nieksztaltek(376);
+		 assertEquals("Result should be one of following",result==876|| result==316 || result==3793 );
+	 	 }
+	 
+	 public void nieksztaltekTest2() throws NieudanyPsikusException{
+		 Integer result=psik.nieksztaltek(111);
+		 assertEquals("Expects same number",result==111);
+	 	 }
 }
