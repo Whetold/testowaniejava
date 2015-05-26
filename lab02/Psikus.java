1718ca32049ec531ba891f52eca9b1ef3cb7fdf1
Whetold <lmielnikowski@sigma.ug.edu.pl>
2015-05-27 00:16:05 +0200

    Update Psikus.java

--- lab02/Psikus.java
+++ lab02/Psikus.java
@@ -1,5 +1,6 @@
 package lab02;
 
+import com.taj.lab02.NieudanyPsikusException;
 public interface Psikus {
 	Integer cyfrokrad(Integer liczba);
 	Integer hultajchochla(Integer liczba) throws NieudanyPsikusException;
